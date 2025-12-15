274604770e7fbcf8ff36fbdd8437d1d209f3777f
FrexB <fupd081022hnenrga2@cbtis217.edu.mx>
2025-12-15 16:07:55 +0100

    BASE DE DATOS COMPLETA

--- Paginas/base.sql
+++ Paginas/base.sql
@@ -16,7 +16,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `bdproyect`
+-- Current Database: `bdproyect`               s
 --
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bdproyect` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
